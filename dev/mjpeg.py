421d6da0cc16d607fdcfb5f8799bdcd5011174ca
cactus1488 <cactus1488@icloud.com>
2024-09-28 18:55:42 +0300

    add ls operation

--- dev/mjpeg.py
+++ dev/mjpeg.py
@@ -3,20 +3,73 @@ import websockets.asyncio.client as websockets
 import mss
 import numpy as np
 import cv2
+import os
 import json
 
-async def capture_and_send():
-    websocket_accept = await websockets.connect('ws://localhost:8000/accept')
-    message = await websocket_accept.recv()
-    data = json.loads(message)
-    session_id = data['id']
-    print(f"Session ID: {session_id}")
 
-    uri = f'ws://localhost:8001/{session_id}'  # WebSocket server URI
-    async with websockets.connect(uri, additional_headers={"X-Will-Stream": True}) as websocket:
+async def fs_commands(ws: websockets.ClientConnection):
+    base = f"/Users/cactus"
+
+    try:
+        while True:
+            message = json.loads(await ws.recv())
+            if message["request"] == "ls":
+                request_id = message["requestId"]
+                path = str(message["path"]).replace("root", base)
+                try:
+                    content = sorted(
+                        [
+                            (
+                                {
+                                    "type": "file",
+                                    "name": entry,
+                                    "bytes": os.path.getsize(f"{path}/{entry}"),
+                                }
+                                if os.path.isfile(f"{path}/{entry}")
+                                else {"type": "folder", "name": entry}
+                            )
+                            for entry in os.listdir(path)
+                        ],
+                        key=lambda x: (x["type"] == "file", x["name"]),
+                    )
+                    await ws.send(
+                        json.dumps(
+                            {
+                                "requestId": request_id,
+                                "event": "ls",
+                                "path": message["path"],
+                                "contents": content,
+                            }
+                        )
+                    )
+                except Exception as err:
+                    print(err)
+                    await ws.send(
+                        json.dumps(
+                            {
+                                "requestId": request_id,
+                                "event": "ls",
+                                "path": message["path"],
+                                "contents": [],
+                            }
+                        )
+                    )
+    except Exception as err:
+        print(err)
+        await ws.close()
+
+
+async def stream(sessionId: str):
+    uri = f"ws://localhost:8001/{sessionId}"  # WebSocket server URI
+    async with websockets.connect(
+        uri, additional_headers={"X-Will-Stream": True}
+    ) as websocket:
         with mss.mss() as sct:
             monitor = sct.monitors[1]  # Capture the first monitor
-            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]  # JPEG quality (0 to 100)
+            encode_param = [
+                int(cv2.IMWRITE_JPEG_QUALITY),
+                70,
+            ]  # JPEG quality (0 to 100)
 
             while True:
                 # Capture the screen
@@ -27,7 +80,7 @@ async def capture_and_send():
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
 
                 # Encode frame as JPEG
-                result, encoded_img = cv2.imencode('.jpg', frame, encode_param)
+                result, encoded_img = cv2.imencode(".jpg", frame, encode_param)
                 if not result:
                     continue
 
@@ -42,6 +95,20 @@ async def capture_and_send():
                     websocket = await websockets.connect(uri)
 
                 # Control frame rate (e.g., 15 FPS)
-                await asyncio.sleep(1/60)
+                await asyncio.sleep(1 / 60)
 
-asyncio.get_event_loop().run_until_complete(capture_and_send())
+
+async def main():
+    websocket_accept = await websockets.connect("ws://localhost:8000/accept")
+    message = await websocket_accept.recv()
+    data = json.loads(message)
+    session_id = data["id"]
+    print(f"Session ID: {session_id}")
+
+    task1 = asyncio.create_task(fs_commands(websocket_accept))
+    task2 = asyncio.create_task(stream(session_id))
+
+    await asyncio.gather(task1, task2)
+
+
+asyncio.run(main())
